089f57e93a5270f5a741392b0c8ba9f548697d22
Kessir wassim <53373646+Muk4g3n@users.noreply.github.com>
2023-12-15 15:17:04 +0100

    added navbar in homepage

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,39 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useUser } from "../hooks";
+// import { useUser } from "../hooks";
 
-import ProfileDropDown from "./ProfileDropDown";
+// import ProfileDropDown from "./ProfileDropDown";
 
 const Navbar = () => {
-  const { user } = useUser();
+  // const { user } = useUser();
   return (
-    <nav className="w-full h-[10vh] bg-[#1E1E2B] text-white flex items-center justify-center gap-[68vw] ">
-      <Link to={"/"}>
-        <div className="h-fit ">Mukagen Merch</div>
-      </Link>
-      {user ? (
-        <ul className="flex items-center justify-around h-16 w-[20%]">
-          <li className="z-20">
-            <Link to={"/create"}>Create design</Link>
-          </li>
-          <li className="z-20">
-            <Link to={"/mydesigns"}>My design</Link>
-          </li>
-          <li className="z-20">
-            <ProfileDropDown />
-          </li>
-        </ul>
+    <nav className="w-full h-[10v] flex items-center justify-center ">
+      <div className="h-fit ">
+        <img src="/logo.svg" alt="logo" />
+        {/* Connected */}
+      </div>
+      <Link to={"/"}>Home</Link>
+      {/* {user ? (
+        <>Ouassim</>
       ) : (
-        <Link
-          type="button"
-          to={"/login"}
-          className="text-white
-          font-medium rounded-full
-          text-sm px-5 py-2.5 mr-2 mb-2 bg-gray-700 hover:bg-gray-600 focus:ring-gray-700   border-gray-700"
-        >
-          Login
-        </Link>
-      )}
+        <>Login</>
+        
+      )} */}
+      <p>Ouassim</p>
     </nav>
   );
 };
--- front/src/pages/Homepage.jsx
+++ front/src/pages/Homepage.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 // import Navbar from "../components/Navbar";
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 // import CanvasModel from "../canvas";
-import { useCanvas } from "../hooks";
+// import { useCanvas } from "../hooks";
 
 const Homepage = () => {
-  const { canvasOptions } = useCanvas();
+  // const { canvasOptions } = useCanvas();
 
-  return <>hello world</>;
+  return (
+    <div className="w-full h-full">
+      <Navbar />
+
+      <div className=" w-full h-[90v] flex">
+        <h1> Side Bar </h1>
+        <h2>Homepage</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
